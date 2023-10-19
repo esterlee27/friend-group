20f88fd0e1288919e6cae51ae657f1432506cd00
Matthew Grindstaff <matt.gstaff@gmail.com>
2023-10-19 10:32:14 +0100

    added data structure

--- group.py
+++ group.py
@@ -2,4 +2,48 @@
 
 # Your code to go here...
 
-my_group =
+
+
+my_group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "connects": {
+            "Zalika": "friend",
+            "John": "partner",
+            },
+        },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "connects": {
+            "Jill": "friend",
+            },
+        },   
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "connects": {
+            "Jill": "partner",
+            },
+        },    
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "connects": {
+            "Zalika": "landlord",
+            "John": "cousin"
+            },
+        },
+    "Ester": {
+        "age": 23,
+        "job": "",
+        "connects": {
+            "Zalika": "landlord",
+            "John": "cousin"
+            },
+        },
+    }
+
+for name, content in my_group.items():
+    print (name)
