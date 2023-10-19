5ae02189f5ef4eea76b414a74a1d6a462a4abdb2
Matthew Grindstaff <matt.gstaff@gmail.com>
2023-10-19 11:38:49 +0100

    added functions

--- group.py
+++ group.py
@@ -9,39 +9,34 @@ my_group = {
         "connects": {
             "Zalika": "friend",
             "John": "partner",
-            },
         },
+    },
     "Zalika": {
         "age": 28,
         "job": "artist",
         "connects": {
             "Jill": "friend",
-            },
-        },   
+        },
+    },
     "John": {
         "age": 27,
         "job": "writer",
         "connects": {
             "Jill": "partner",
-            },
-        },    
+        },
+    },
     "Nash": {
         "age": 34,
         "job": "chef",
-        "connects": {
-            "Zalika": "landlord",
-            "John": "cousin"
-            },
-        },
+        "connects": {"Zalika": "landlord", "John": "cousin"},
+    },
     "Ester": {
         "age": 23,
         "job": "",
-        "connects": {
-            "Zalika": "landlord",
-            "John": "cousin"
-            },
-        },
-    }
+        "connects": {"Zalika": "landlord", "John": "cousin"},
+    },
+}
+
 
 def forget(person1, person2):
     for key in my_group.keys():
@@ -52,18 +47,11 @@ def forget(person1, person2):
 
 
 def add_person(name, age, job, relations):
-    my_group[name] = {
-        "age": age,
-        "job": job,
-        "connects": relations
-    }
+    my_group[name] = {"age": age, "job": job, "connects": relations}
 
-add_person("Frosty", 23, "", {"Zalika": "landlord", "John": "cousin"})
 
 def average_age(group):
     total = 0
     for person in group.values():
         total += person["age"]
     return total / len(group)
-
-print(average_age(my_group))
